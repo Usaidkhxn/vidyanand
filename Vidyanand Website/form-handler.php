b3d0b09f80e00b79b8f5d77d6dbf7bc6c8eb6e9a
Usaidkhxn <97670085+Usaidkhxn@users.noreply.github.com>
2022-01-13 17:33:32 +0530

    Add files via upload

---
+++ Vidyanand Website/form-handler.php
@@ -0,0 +1,30 @@
+<?php
+$name = $_POST['Name']
+$visitor_email = $_POST['Email']
+$subject = $_POST['Subject']
+$message = $_POST['Message']
+
+
+$email_from = 'usaiddgr8@gmail.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n"
+"User Email: $visitor_email.\n"
+"Subject: $subject.\n"
+"User Message: $message.\n";
+
+
+$to = 'nasimkhansir@gmail.com';
+
+$headers = "From: $email_from\r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+
+
+?>
